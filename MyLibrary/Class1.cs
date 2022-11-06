485fa0d45fcbdbb3c31f3681e25f68ec4d91ac02
rajath89 <nagarajathsm@gmail.com>
2022-11-06 14:17:18 +0530

    Initial commit

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+namespace MyLibrary;
+//dotnet add package BenchmarkDotNet --version 0.13.2
+public class Class1
+{
+
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//dotnet new -o MyLibrary . dotnet new sln -n "MySolution"
+using MyLibrary;
+string str= "hello";
+var dt = new Dictionary<string,string>();
+Console.WriteLine("Hello, World!");
