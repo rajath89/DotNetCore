4d9ee552d59bd356f4a4b40135b711cadf6ee50d
rajath89 <nagarajathsm@gmail.com>
2023-01-07 17:23:56 +0530

    TypeCasting example

---
+++ MyLib/Customer/TypeCastExample/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.TypeCastExample
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustormerName { get; set; }
+
+        public Customer(int custId, string custName)
+        {
+            CustomerId = custId;
+            CustormerName = custName;
+        }
+
+    }
+}
---
+++ MyLib/Customer/TypeCastExample/EliteCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.TypeCastExample
+{
+    public class EliteCustomer : Customer
+    {
+        public int CouponsOwned { get; set; }
+
+        public EliteCustomer(int custId, string custName, int coupons) : base(custId, custName)
+        {
+            CouponsOwned = coupons;
+        }
+    }
+}
