3809c3fb962c54521777af567985f4d3a3e39b57
rajath89 <nagarajathsm@gmail.com>
2023-07-22 14:08:31 +0530

    Design pattern : Singleton & SOLID : SingleResp

---
+++ MyLib/advancedCS/DesignPatterns/Singleton.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.advancedCS.DesignPatterns
+{
+    //No Thread Safe Singleton
+    public sealed class Singleton1
+    {
+        private Singleton1() { }
+        private static Singleton1 instance = null;
+        public static Singleton1 Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton1();
+                }
+                return instance;
+            }
+        }
+    }
+
+    //Thread Safety Singleton
+    public sealed class Singleton2
+    {
+        Singleton2() { }
+        private static readonly object Lock = new object();
+        private static Singleton2 instance = null;
+        public static Singleton2 Instance
+        {
+            get
+            {
+                lock (Lock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton2();
+                        }
+                        return instance;
+                    }
+            }
+        }
+    }
+
+    //Thread Safety Singleton using Double-Check Locking
+    public sealed class Singleton3
+    {
+        Singleton3() { }
+        private static readonly object Lock = new object ();
+    private static Singleton3 instance = null;
+        public static Singleton3 Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (Lock)
+                        {
+                            if (instance == null)
+                            {
+                                instance = new Singleton3();
+                            }
+                        }
+                }
+                return instance;
+            }
+        }
+    }
+
+    //Thread Safe Singleton without using locks and no lazy instantiation
+    public sealed class Singleton4
+    {
+        private static readonly Singleton4 instance = new Singleton4();
+        static Singleton4()
+        {
+        }
+        private Singleton4()
+        {
+        }
+        public static Singleton4 Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+
+    //Using .NET 4's Lazy<T> type
+    public sealed class Singleton5
+    {
+        private Singleton5()
+        {
+        }
+        private static readonly Lazy<Singleton5> lazy = new Lazy<Singleton5>(() => new Singleton5());
+        public static Singleton5 Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+    }
+
+    internal class Singleton
+    {
+        public static void TestExample()
+        {
+
+        }
+    }
+}
---
+++ MyLib/advancedCS/SOLID_Principles/SingleResponsibility.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.advancedCS.SOLID_Principles
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailId { get; set; }
+        public string Address { get; set; }
+
+        public Customer(string customerId, string customerName, string contactNumber, string emailId, string address)
+        {
+            this.CustomerId = customerId;
+            this.CustomerName = customerName;
+            this.ContactNumber = contactNumber;
+            this.EmailId = emailId;
+            this.Address = address;
+        }
+
+        public string GetCustomerDetails()
+        {
+            string details = string.Format("{0, -15}: {1}\n{2, -15}: {3}\n{4, -15}: {5}\n" +
+                 "{6, -15}: {7}\n{8, -15}: {9}",
+                 "Customer Id", this.CustomerId, "Customer Name", this.CustomerName,
+                 "ContactNumber", this.ContactNumber, "Email Id", this.EmailId,
+                 "Address", this.Address);
+            return details;
+        }
+        public string UpdateContactDetails(string contactNumber, string emailId, string address)
+        {
+            if (contactNumber != null)
+            {
+                this.ContactNumber = contactNumber;
+            }
+            if (emailId != null)
+            {
+                this.EmailId = emailId;
+            }
+            if (address != null)
+            {
+                this.Address = address;
+            }
+            return "\nContact Details UPDATED successfully!";
+        }
+    }
+
+    public class PersistCustomerDetails
+    {
+        public Customer Customer { get; set; }
+        public PersistCustomerDetails(Customer customer)
+        {
+            this.Customer = customer;
+        }
+        public string SaveCustomerDetails()
+        {
+            string path = @"D:\CustomerDetails.txt";
+            FileStream fileStream = null;
+            bool result = File.Exists(path);
+            if (result)
+                fileStream = new FileStream(path, FileMode.Open, FileAccess.Write);
+            else
+                fileStream = new FileStream(path, FileMode.CreateNew, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fileStream);
+            writer.WriteLine("{0}, {1}, {2}, {3}, {4}",
+                Customer.CustomerId, Customer.CustomerName, Customer.ContactNumber,
+                Customer.EmailId, Customer.Address);
+            writer.Close();
+            return "\nDetails SAVED successfully in a file!\n";
+        }
+    }
+
+        internal class SingleResponsibility
+        {
+            public static void TestSingleResponsibility()
+            {
+                Customer customer = new Customer("C101", "Jenny", "9845054609", "jenny@gmail.com",
+                    "Maple street, 5th cross road");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("           Customer Details           ");
+                Console.WriteLine("--------------------------------------");
+                string customerDetails = customer.GetCustomerDetails();
+                Console.WriteLine(customerDetails);
+                string updateStatus = customer.UpdateContactDetails("9995094609",
+                        "jenny123@gmail.com", "Ivory Street, 10th Block");
+                Console.WriteLine(updateStatus);
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("       Updated Customer Details       ");
+                Console.WriteLine("--------------------------------------");
+                customerDetails = customer.GetCustomerDetails();
+                Console.WriteLine(customerDetails);
+                PersistCustomerDetails saveCustomerDetails = new PersistCustomerDetails(customer);
+                string saveStatus = saveCustomerDetails.SaveCustomerDetails();
+                Console.WriteLine(saveStatus);
+        }
+        }
+}
